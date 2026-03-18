fe5226ca4b42e242e6c19fec4b569f0b028aa309
RaniVai <bierania54081@gmail.com>
2026-03-18 15:23:18 +0900

    新たなインゴットの実装

---
+++ Content/Items/Placeable/Item_R001.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace testmod4.Content.Items.Placeable
+{
+    public class Item_R001 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 25;
+            ItemID.Sets.SortingPriorityMaterials[Item.type] = 59; // Influences the inventory sort order. 59 is PlatinumBar, higher is more valuable.
+
+            // The Chlorophyte Extractinator can exchange items. Here we tell it to allow a one-way exchanging of 5 ExampleBar for 2 ChlorophyteBar.
+            ItemTrader.ChlorophyteExtractinator.AddOption_OneWay(Type, 5, ItemID.ChlorophyteBar, 2);
+        }
+
+        public override void SetDefaults()
+        {
+            // ModContent.TileType returns the ID of the tile that this item should place when used. ModContent.TileType<T>() method returns an integer ID of the tile provided to it through its generic type argument (the type in angle brackets)
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Item_R001>());
+            Item.width = 20;
+            Item.height = 20;
+            Item.value = 750; // The cost of the item in copper coins. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Tiles/Item_R001.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader; // 追加
+using Terraria.ObjectData;
+using Terraria.ID;
+
+
+namespace testmod4.Content.Tiles
+{
+    public class Item_R001 : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.MetalBar")); // localized text for "Metal Bar"
+        }
+
+
+    }
+}
